8865198c5d0af4c16d9a8566c18d992df65660ff
JesusL215 <jesus.jli215@gmail.com>
2026-07-01 23:58:23 -0500

    agregando cambios preliminares

--- frontend/src/main/java/com/smartpark/estacionamiento/patrones/comportamiento/state/SlotState.java
+++
@@ -1,7 +0,0 @@
-package com.smartpark.estacionamiento.patrones.comportamiento.state;
-import com.smartpark.estacionamiento.model.domain.ParkingSlot;
-public interface SlotState {
-    void ocupar(ParkingSlot slot);
-    void liberar(ParkingSlot slot);
-    String getEstado();
-}
